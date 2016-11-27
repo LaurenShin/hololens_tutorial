07de182b9eda517e208468cc4c35e1ee252bda39
LaurenShin <soqpvm3@gmail.com>
2016-11-27 22:04:11 +0900

    Initial commit

---
+++ Assets/DestroyByContent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContent : MonoBehaviour {
+
+    // Use this for initialization
+    void OnTriggerEnter(Collider other) {
+        Debug.Log(other.name);
+        if (other.tag == "Boundary") {
+            return;        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
